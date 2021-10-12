ca21952b910f64751d29456cedd29c52a07b155c
Dean-Fitzpatrick <71977233+Dean-Fitzpatrick@users.noreply.github.com>
2021-10-12 11:28:22 +0100

    Week 2 Assignment Solutions

---
+++ Assignment 1 module 3/Assignement 1 Queries.sql
@@ -0,0 +1,9 @@
+select * from Employee; -- Display Records in Employee Table
+
+select * from ResourceTbl; -- Display Records in Resource Table
+
+select * from EventRequest; -- Display Records in EventRequest Table
+
+select * from EventPlan; -- Display Records in EventPlan Table
+
+select * from EventPlanLine -- Display Records in EventPlanLine Table
